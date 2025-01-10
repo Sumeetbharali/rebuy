83b269347a649e5f7e6eff193821b645723dc61f
mohamedhalem909@gmail.com <mohamedhalem909@gmail.com>
2025-01-10 06:59:15 +0200

    in constatn added required text styles and colors

--- lib/src/core/constants/colors.dart
+++ lib/src/core/constants/colors.dart
@@ -6,6 +6,7 @@ class AppColor {
   static Color black = const Color(0xFF000000);
   static Color white = const Color(0xFFFFFFFF);
   static Color lightestGray = const Color(0xFFf5f5f5);
+  static Color softGray = const Color(0xFFD9D9D9);
   static Color dustyRose = const Color(0xFFc1839f);
   static Color darkGray = const Color(0xFF3c3c3c);
   static Color vibrantRed = const Color(0xFFff5a5f);
