6ce1e3777abb1b49549f79b9a9bcf2dff899561e
mohamedhalem909@gmail.com <mohamedhalem909@gmail.com>
2025-01-10 06:58:29 +0200

    added app theme to override scafold background

--- lib/re_buy_app.dart
+++ lib/re_buy_app.dart
@@ -1,3 +1,5 @@
+import 'package:rebuy/src/core/theme/theme.dart';
+
 import 'src/core/config/config.dart';
 import 'package:flutter/material.dart';
 import 'src/core/routes/routes.dart';
@@ -12,8 +14,9 @@ class ReBuyApp extends StatelessWidget {
       designSize: const Size(414, 896),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, ch) => const DismissKeyboard(
+      builder: (context, ch) => DismissKeyboard(
         child: MaterialApp(
+          theme: AppThemes.defaultTheme,
           debugShowCheckedModeBanner: false,
           initialRoute: RoutesName.initial,
           onGenerateRoute: AppRoute.generate,
